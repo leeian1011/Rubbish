5264b96a8041bd119c14f2e014d0135fbd620340
Fungus <fungus@LAPTOP-MOUHRJJB>
2025-02-04 00:10:11 +0800

    feat: exit done

--- src/buildins/exit.c
+++ src/buildins/exit.c
@@ -6,14 +6,15 @@
 /*   By: jianwong <jianwong@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/21 18:56:26 by jianwong          #+#    #+#             */
-/*   Updated: 2025/01/21 18:59:53 by jianwong         ###   ########.fr       */
+/*   Updated: 2025/02/04 00:10:00 by jianwong         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/buildins.h"
 
+// exit code will need to be that last command executed
 void	exit(int exit_code)
 {
 	printf("exit\n");
-	exit(127);
+	exit(exit_code);
 }
